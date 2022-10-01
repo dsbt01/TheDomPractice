7f40cbb8d72cf609a42847651db108824732aced
David Suarez <dsuarez001@att.net>
2022-09-30 20:38:50 -0400

    partial save

--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -1,5 +1,13 @@
+//var numberToWords = require('number-to-words');
+
+var counter = 3;
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
+	let elem = document.createElement('li');
+	counter += 1;
+	elem.innerHTML = `${numberToWords.toWordsOrdinal(counter)} element`;
 
+	let listToAdd = document.getElementById('myList');
+	listToAdd.appendChild(elem);
 });
