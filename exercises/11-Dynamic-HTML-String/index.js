f215e0086c9f7627eef4f7d25e3311cf96880c70
David Suarez <dsuarez001@att.net>
2022-10-03 17:56:39 -0400

    ex 11

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,2 +1,5 @@
-let myString = "<p>Hello!</p> <strong>My friend</strong>, ";
+const d = new Date();
+let year = d.getFullYear();
+
+let myString = "<p>Hello!</p> <strong>My friend</strong>, we are in the year " + year;
 document.write(myString);
