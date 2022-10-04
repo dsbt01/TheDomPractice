71e182a052dcbcdb9e536c11943a64871e2a08d5
David Suarez <dsuarez001@att.net>
2022-10-03 20:46:01 -0400

    ex 12

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,2 +1,14 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
+select = document.getElementById('mySelect');
+
+for (var i = 0; i < countries.length; i++){
+    var opt = document.createElement('option');
+    opt.value = countries[i].toLowerCase();
+    opt.innerHTML = countries[i];
+    select.appendChild(opt);
+}
+
+select.addEventListener('change',function(e){
+    alert(e.target.selectedOptions[0].text);
+});
