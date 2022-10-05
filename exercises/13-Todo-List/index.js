fccd8adf2e14e6cfe46afd535a660a4d88b3ecc2
David Suarez <dsuarez001@att.net>
2022-10-05 09:20:42 -0400

    event is created for the add

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,8 @@
 // Your code here
+var element = document.getElementById("addToDo");
+element.addEventListener("keypress", function(event) {
+	 if (event.key === "Enter") {
+ 		alert(event.key);
+ 		event.preventDefault();
+     }
+});
