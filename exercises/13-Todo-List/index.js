c406a018f7968fb929eceabd1134880f60ee68ee
David Suarez <dsuarez001@att.net>
2022-10-05 19:30:54 -0400

    partial save

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,15 +1,21 @@
 // Your code here
-var element = document.getElementById("addToDo");
-element.addEventListener("keypress", function(event) {
+var elementTextBox = document.getElementById("addToDo");
+elementTextBox.addEventListener("keypress", function(event) {
 	 if (event.key === "Enter") {
-        let elem = document.createElement('li');
+        let liElem = document.createElement('li');
         let spamElement = document.createElement('span');
-        spamElement.appendChild(document.createTextNode(element.value));
-        elem.appendChild(spamElement);
-        elem.appendChild(document.createTextNode(element.value));
+
+        var trashIcon = document.createElement("i");
+        trashIcon.className="fa fa-trash";
+        spamElement.appendChild(trashIcon);
+
+        liElem.appendChild(spamElement);
+
+        liElem.appendChild(document.createTextNode(elementTextBox.value));
+        
         let aList = document.getElementsByTagName ('ul')[0];
-        aList.append(elem);
-        element.value = "";
+        aList.append(liElem);
+        elementTextBox.value = "";
  		event.preventDefault();
      }
 });
