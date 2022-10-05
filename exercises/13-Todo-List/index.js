5a8262314735e53abf736277ddc304b7ae4b9b34
David Suarez <dsuarez001@att.net>
2022-10-05 09:54:28 -0400

    partial save

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -3,6 +3,9 @@ var element = document.getElementById("addToDo");
 element.addEventListener("keypress", function(event) {
 	 if (event.key === "Enter") {
         let elem = document.createElement('li');
+        let spamElement = document.createElement('span');
+        spamElement.appendChild(document.createTextNode(element.value));
+        elem.appendChild(spamElement);
         elem.appendChild(document.createTextNode(element.value));
         let aList = document.getElementsByTagName ('ul')[0];
         aList.append(elem);
